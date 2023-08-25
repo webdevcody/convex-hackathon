d1a3f443847500253c91acf027e936752bcaaef6
Web Dev Cody <webdevcody@gmail.com>
2023-08-24 23:17:11 -0400

    fixing type error

--- src/app/resources/page.tsx
+++ src/app/resources/page.tsx
@@ -67,9 +67,7 @@ export default function Participants() {
           height="315"
           src="https://www.youtube.com/embed/5cGdBDXqCJ0?si=SFJjznLZwBUpZrTQ"
           title="YouTube video player"
-          frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowfullscreen
         ></iframe>
 
         <iframe
@@ -77,9 +75,7 @@ export default function Participants() {
           height="315"
           src="https://www.youtube.com/embed/4DEFIEHbC_s?si=vcoB9wI8EK6B8ijQ"
           title="YouTube video player"
-          frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowfullscreen
         ></iframe>
       </div>
     </div>
