05ff90dd430328651e3aa6e3e5aa593403c96cbf
Web Dev Cody <webdevcody@gmail.com>
2023-09-19 23:47:48 -0400

    update generated code

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -14,6 +14,7 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
+import type * as csv from "../csv";
 import type * as participants from "../participants";
 
 /**
@@ -25,6 +26,7 @@ import type * as participants from "../participants";
  * ```
  */
 declare const fullApi: ApiFromModules<{
+  csv: typeof csv;
   participants: typeof participants;
 }>;
 export declare const api: FilterApi<
